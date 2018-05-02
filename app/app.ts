e8b731bf26a2502e956108d49cfa470f979bd512
Jatin Sanghvi <jatin085@yahoo.com>
2018-05-02 12:03:52 -0700

    Follow Demo 'Using let and const with Type Annotations'

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,16 @@
 function startGame() {
     // Starting a new game.
+
+    let playerName: string = 'Audrey';
+    logPlayer(playerName);
+
     var messageElement = document.getElementById('messages');
     messageElement.innerText = 'Welcome to MultiMath! Starting new game...';
     console.log('Starting a new game.');
+}
+
+function logPlayer(name) {
+    console.log(`Starting a new game for player: ${name}.`);
 }
 
 document.getElementById('startGame').addEventListener('click', startGame);
