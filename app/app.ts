63abbb0410af1414c9e17eb52dbc1a0c75ddd477
Jatin Sanghvi <jatin085@yahoo.com>
2018-05-02 17:08:45 -0700

    Follow Demo 'Adding Type Annotations and Default Parameter Values'

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,22 @@
 function startGame() {
     // Starting a new game.
-    const playerName: string = 'Audrey';
+    const playerName: string | undefined = getInputValue('playername');
     logPlayer(playerName);
-
-    const messagesElement: HTMLElement | null = document.getElementById('messages');
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+    postScore(100, playerName);
 }
 
-function logPlayer(name) {
+function logPlayer(name: string = 'MultiMath Player'): void {
     console.log(`Starting a new game for player: ${name}.`);
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+    let scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+}
+
+function getInputValue(elementId: string): string | undefined {
+    let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
+    return (inputElement.value === '') ? undefined : inputElement.value;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
